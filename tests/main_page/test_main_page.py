fb1786d34a81ef55fbe77512bbe32d069839e808
Xenocode <maxikote2012@gmail.com>
2023-03-25 15:17:19 +0300

    added test_display_footer

--- tests/main_page/test_main_page.py
+++ tests/main_page/test_main_page.py
@@ -27,3 +27,17 @@ def test_display_links(chrome_driver: webdriver):
     links = chrome_driver.find_elements(By.TAG_NAME, "a")
     for link in links:
         assert link.is_displayed()
+
+#Тест на наличие футера
+def test_display_footer(chrome_driver: webdriver):
+    chrome_driver.get('http://the-internet.herokuapp.com/')
+
+    footer_text_1 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="page-footer"]/div/div[text()="Powered by "]'))
+    )
+    assert footer_text_1 is not None
+
+    footer_text_2 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="page-footer"]/div/div/a[text()="Elemental Selenium"]'))
+    )
+    assert footer_text_2 is not None
