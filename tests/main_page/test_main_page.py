90fe9188ecd0f517af92012aac4788eca0e9574c
Xenocode <maxikote2012@gmail.com>
2023-03-25 15:04:01 +0300

    added test_on_welcome

--- tests/main_page/test_main_page.py
+++ tests/main_page/test_main_page.py
@@ -9,3 +9,13 @@ from selenium.webdriver.common.by import By
 def test_open_main_page(chrome_driver: webdriver):
     chrome_driver.get('http://the-internet.herokuapp.com/')
     assert "The Internet" in chrome_driver.title
+
+#Тест на присутствия приветствия на странице
+def test_on_welcome(chrome_driver: webdriver):
+    chrome_driver.get('http://the-internet.herokuapp.com/')
+
+    welcome_text = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="content"]/h1'))
+    )
+
+    assert welcome_text.text == "Welcome to the-internet"
