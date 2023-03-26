2e98ce6cffbc2d9bd4ac2c9a8ce3401cc8d5e528
Xenocode <maxikote2012@gmail.com>
2023-03-26 17:33:16 +0300

    added test_selected_checkboxes

--- tests/testing_checkboxes/test_checkboxes.py
+++ tests/testing_checkboxes/test_checkboxes.py
@@ -59,3 +59,22 @@ def test_empty_checkboxes(chrome_driver: webdriver):
     checkbox_2.click()
 
     assert not checkbox_2.is_selected()
+
+#Тест на отмеченные чекбоксы
+def test_selected_checkboxes(chrome_driver: webdriver):
+
+    chrome_driver.get('http://the-internet.herokuapp.com/checkboxes')
+
+    checkbox_1 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="checkboxes"]/input[1]'))
+    )
+
+    checkbox_1.click()
+
+    assert checkbox_1.is_selected()
+
+    checkbox_2 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="checkboxes"]/input[2]'))
+    )
+
+    assert checkbox_2.is_selected()
