7270053580b302128b523d9fc7be1eb0fdbf5665
Xenocode <maxikote2012@gmail.com>
2023-03-26 17:26:23 +0300

    added test_default_checkboxes

--- tests/testing_checkboxes/test_checkboxes.py
+++ tests/testing_checkboxes/test_checkboxes.py
@@ -20,8 +20,6 @@ def test_open_checkboxes(chrome_driver: webdriver):
 #Тест корректности заголовка
 def test_correct_title(chrome_driver: webdriver):
 
-    chrome_driver.get('http://the-internet.herokuapp.com/checkboxes')
-
     correct_title = "Checkboxes"
 
     title = WebDriverWait(chrome_driver, 10).until(
@@ -29,3 +27,18 @@ def test_correct_title(chrome_driver: webdriver):
     )
 
     assert title.text == correct_title
+
+#Тест на отобрежение чекбоксов по умолчанию
+def test_default_checkboxes(chrome_driver: webdriver):
+
+    checkbox_1 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="checkboxes"]/input[1]'))
+    )
+
+    assert not checkbox_1.is_selected()
+
+    checkbox_2 = WebDriverWait(chrome_driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="checkboxes"]/input[2]'))
+    )
+
+    assert checkbox_2.is_selected()
