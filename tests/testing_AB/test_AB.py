146903565cd2cd5b42e98b627dbadde1a69a4490
Xenocode <maxikote2012@gmail.com>
2023-03-26 17:42:43 +0300

    fix test_correct_title

--- tests/testing_AB/test_AB.py
+++ tests/testing_AB/test_AB.py
@@ -28,14 +28,3 @@ def test_correct_title(chrome_driver: webdriver):
     )
 
     assert title.text == version_A or title.text == version_B
-
-#Тест описания вариации A/B
-def test_ab_description(chrome_driver: webdriver):
-    
-    description_A = "Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through)."
-
-    description = WebDriverWait(chrome_driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id="content"]/div/p'))
-    )
-
-    assert description.text == description_A
